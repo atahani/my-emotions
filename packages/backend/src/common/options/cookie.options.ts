fe3d97c16bcfcaafc7234d94f682238e97326eaa
Ahmad Tahani <ahmad.tahani@gmail.com>
2020-12-21 10:25:29 +0330

    set default max age cookie as 30 days

--- packages/backend/src/common/options/cookie.options.ts
+++ packages/backend/src/common/options/cookie.options.ts
@@ -5,4 +5,5 @@ export const getDefaultCookieOptions = (): CookieOptions => ({
     path: process.env.API_PREFIX || '/graphql',
     secure: !process.env.APP_ENVIRONMENT || process.env.APP_ENVIRONMENT === 'local_development' ? false : true,
     sameSite: !process.env.APP_ENVIRONMENT || process.env.APP_ENVIRONMENT === 'local_development' ? undefined : 'none',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
 })
