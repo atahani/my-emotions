ea39aa5482e0a0a779634f00088081bba4e7ab0d
Ahmad Tahani <ahmad.tahani@gmail.com>
2020-12-28 09:20:43 +0330

    add default values for google passport env

--- packages/backend/src/config/auth.ts
+++ packages/backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
-    googleClientId: process.env.PASSPORT_GOOGLE_CLIENT_ID,
-    googleSecret: process.env.PASSPORT_GOOGLE_SECRET,
+    googleClientId: process.env.PASSPORT_GOOGLE_CLIENT_ID || 'google_client_id',
+    googleSecret: process.env.PASSPORT_GOOGLE_SECRET || 'google_secret',
     googleCallbackURL: process.env.PASSPORT_GOOGLE_CALLBACK_URL || 'http://localhost:5050/auth/google/redirect',
     tokenExpiresInMin: Number(process.env.TOKEN_EXPIRE_IN_MIN || 90),
 }
